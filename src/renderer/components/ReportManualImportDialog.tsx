b71fd914b79601095e60a10f52598016242e64ff
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-03-13 21:43:28 +0100

    Validation for manual report import dialog (#28)

--- src/renderer/components/ReportManualImportDialog.tsx
+++ src/renderer/components/ReportManualImportDialog.tsx
@@ -5,6 +5,9 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormControl,
+  MenuItem,
+  Select,
   Stack,
   TextField,
 } from '@mui/material'
@@ -17,15 +20,31 @@ type ReportManualImportDialogProps = {
   onClose: () => void
 }
 
+const isValidIncomeDate = (input: string) => {
+  return /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(input)
+}
+
+const isValidCurrencyCode = (currencyCode: string) => {
+  return currencyCode != ''
+}
+
+const isValidIncomeAmount = (amount: number) => {
+  return Number.isFinite(amount) && amount != 0
+}
+
+const isValidWhtAmount = (amount: number) => {
+  return Number.isFinite(amount)
+}
+
 export const ReportManualImportDialog = (props: ReportManualImportDialogProps) => {
   const queryClient = useQueryClient()
-  const [incomeType, setIncomeType] = useState('') // TODO: dropdown
+  const [incomeType, setIncomeType] = useState<PassiveIncomeType>('dividend')
   const [payingEntity, setPayingEntity] = useState('')
-  const [incomeDate, setIncomeDate] = useState('') // TODO: validate date
-  const [incomeCurrencyCode, setIncomeCurrencyCode] = useState('') // TODO: dropdown
+  const [incomeDate, setIncomeDate] = useState('')
+  const [incomeCurrencyCode, setIncomeCurrencyCode] = useState('')
   const [incomeCurrencyAmount, setIncomeCurrencyAmount] = useState('') // TODO: validate number
-  const [whtCurrencyCode, setWhtCurrencyCode] = useState('') // TODO: dropdown + same-as-income toggle
-  const [whtCurrencyAmount, setWhtCurrencyAmount] = useState('') // TODO: validate number
+  const [whtCurrencyCode, setWhtCurrencyCode] = useState('EUR')
+  const [whtCurrencyAmount, setWhtCurrencyAmount] = useState('0')
   return <Dialog
       open
       onClose={props.onClose}
@@ -36,12 +55,19 @@ export const ReportManualImportDialog = (props: ReportManualImportDialogProps) =
     <DialogContent>
       <p style={{ marginBottom: 0}}></p>
       <Stack gap={1}>
-        <TextField
-          style={{ width: 400 }}
-          label="Income Type (dividend or interest)"
-          value={incomeType}
-          onChange={e => setIncomeType(e.target.value)}
-        />
+        <FormControl size="small">
+          <Select
+            value={incomeType}
+            sx={{ minWidth: 90 }}
+            onChange={async e => {
+              const newIncomeType = e.target.value as PassiveIncomeType
+              setIncomeType(newIncomeType)
+            }
+          }>
+            <MenuItem value='dividend'>Dividend</MenuItem>
+            <MenuItem value='income'>Income</MenuItem>
+          </Select>
+        </FormControl>
         <TextField
           style={{ width: 400 }}
           label="Paying Entity (optional)"
@@ -52,30 +78,35 @@ export const ReportManualImportDialog = (props: ReportManualImportDialogProps) =
           style={{ width: 400 }}
           label="Income Date (YYYY-MM-DD)"
           value={incomeDate}
+          error={!isValidIncomeDate(incomeDate)}
           onChange={e => setIncomeDate(e.target.value)}
         />
         <TextField
           style={{ width: 400 }}
           label="Income Currency Code"
           value={incomeCurrencyCode}
+          error={!isValidCurrencyCode(incomeCurrencyCode)}
           onChange={e => setIncomeCurrencyCode(e.target.value)}
         />
         <TextField
           style={{ width: 400 }}
           label="Income Amount in Currency"
           value={incomeCurrencyAmount}
+          error={!isValidIncomeAmount(parseFloat(incomeCurrencyAmount))}
           onChange={e => setIncomeCurrencyAmount(e.target.value)}
         />
         <TextField
           style={{ width: 400 }}
           label="Withholding Tax Currency Code"
           value={whtCurrencyCode}
+          error={!isValidCurrencyCode(whtCurrencyCode)}
           onChange={e => setWhtCurrencyCode(e.target.value)}
         />
         <TextField
           style={{ width: 400 }}
           label="Withholding Tax Amount in Currency"
           value={whtCurrencyAmount}
+          error={!isValidWhtAmount(parseFloat(whtCurrencyAmount))}
           onChange={e => setWhtCurrencyAmount(e.target.value)}
         />
       </Stack>
@@ -83,19 +114,30 @@ export const ReportManualImportDialog = (props: ReportManualImportDialogProps) =
     <DialogActions>
       <ButtonGroup>
       <Button onClick={props.onClose}>Cancel</Button>
-      <Button variant="contained" onClick={async () => {
-        await ipcContextApi.importTrivialReport({
-            type: incomeType as PassiveIncomeType,
+      <Button
+        variant="contained"
+        disabled={!(
+          isValidIncomeDate(incomeDate) &&
+          isValidCurrencyCode(incomeCurrencyCode) &&
+          isValidIncomeAmount(parseFloat(incomeCurrencyAmount)) &&
+          isValidCurrencyCode(whtCurrencyCode) &&
+          isValidWhtAmount(parseFloat(whtCurrencyAmount))
+        )}
+        onClick={async () => {
+          await ipcContextApi.importTrivialReport({
+            type: incomeType,
             payingEntity,
             incomeDate,
             incomeCurrencyCode,
             incomeCurrencyAmount: parseFloat(incomeCurrencyAmount),
             whtCurrencyCode,
             whtCurrencyAmount: parseFloat(whtCurrencyAmount),
-        })
-        queryClient.invalidateQueries({ queryKey: ['reports'] })
-        props.onClose()
-      }} autoFocus>
+          })
+          queryClient.invalidateQueries({ queryKey: ['reports'] })
+          props.onClose()
+        }}
+        autoFocus
+      >
         Save
       </Button>
       </ButtonGroup>

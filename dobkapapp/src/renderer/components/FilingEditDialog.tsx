6f223212960175974a2215d173b65d4fad768aa5
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-07-16 19:59:03 +0200

    Filing status in filing edit dialog

--- dobkapapp/src/renderer/components/FilingEditDialog.tsx
+++ dobkapapp/src/renderer/components/FilingEditDialog.tsx
@@ -1,5 +1,5 @@
 import { formatRsdcAmount } from '../../common/helpers'
-import { Filing } from '../../common/ipc-types'
+import { Filing, FilingStatus } from '../../common/ipc-types'
 import {
   Button,
   ButtonGroup,
@@ -7,6 +7,9 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormControl,
+  MenuItem,
+  Select,
   Stack,
   TextField,
 } from '@mui/material'
@@ -21,6 +24,7 @@ type FilingEditDialogProps = {
 
 export const FilingEditDialog = (props: FilingEditDialogProps) => {
   const queryClient = useQueryClient()
+  const [filingStatus, setFilingStatus] = useState(props.filing.status)
   const [paymentReference, setPaymentReference] = useState(props.filing.taxPaymentReference)
   return <Dialog
       open
@@ -32,6 +36,16 @@ export const FilingEditDialog = (props: FilingEditDialogProps) => {
     <DialogContent>
       <p style={{ marginBottom: 0}}></p>
       <Stack gap={1}>
+        <FormControl size="small">
+          <Select
+            value={filingStatus}
+            sx={{ minWidth: 90 }}
+            onChange={e => setFilingStatus(e.target.value as FilingStatus)}>
+            <MenuItem value='init'>Initial</MenuItem>
+            <MenuItem value='filed'>Filed</MenuItem>
+            <MenuItem value='paid'>Paid</MenuItem>
+          </Select>
+        </FormControl>
         <TextField
           style={{ width: 400 }}
           disabled
@@ -50,7 +64,12 @@ export const FilingEditDialog = (props: FilingEditDialogProps) => {
           style={{ width: 400 }}
           label="Payment Reference"
           value={paymentReference}
-          onChange={e => setPaymentReference(e.target.value)}
+          onChange={e => {
+            setPaymentReference(e.target.value)
+            if (filingStatus == 'init' && e.target.value != '') {
+              setFilingStatus('filed')
+            }
+          }}
         />
       </Stack>
     </DialogContent>
@@ -60,10 +79,8 @@ export const FilingEditDialog = (props: FilingEditDialogProps) => {
       <Button variant="contained" onClick={async () => {
         await ipcContextApi.updateFiling({
           ...props.filing,
+          status: filingStatus,
           taxPaymentReference: paymentReference,
-          status: (props.filing.status == 'init' && paymentReference != '')
-            ? 'filed'
-            : props.filing.status
         })
         queryClient.invalidateQueries({ queryKey: ['filings'] })
         props.onClose()
